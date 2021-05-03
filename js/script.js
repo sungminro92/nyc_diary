03a353fe4ba07bdcead663465e1854c76b3a54d7
sungminro <sungminro92@gmail.com>
2021-05-03 14:49:38 -0400

    played with html and js and cartojs, didn't work

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
+let manholeLayer;
+let eventLayer;
+let placeLayer;
+let foodLayer;
+
 window.onload = (event) => {
   console.log("page connected with JS");
-  cartodb.createVis('map', 'http://thenewschool.carto.com/u/tjdals504/api/v2/viz/b0d53334-4181-44e0-9942-285c59f4da19/viz.json', {
+
+  let url = 'http://thenewschool.carto.com/u/tjdals504/api/v2/viz/b0d53334-4181-44e0-9942-285c59f4da19/viz.json'
+
+
+  cartodb.createVis('map', url, {
     shareable: true,
     title: true,
     description: true,
@@ -14,10 +23,35 @@ window.onload = (event) => {
     .done(function(vis, layers) {
       // layer 0 is the base layer, layer 1 is cartodb layer
       // when setInteraction is disabled featureOver is triggered
+      // let manholeLayer = layers[1];
+      // document.getElementById('manholes')
+      let mapLayers = vis.getLayers();
+      console.log(mapLayers);
+      
       layers[1].setInteraction(true);
-      layers[1].on('featureOver', function(e, latlng, pos, data, layerNumber) {
-        console.log(e, latlong, pos, data, layerNumber);
+      // manholeLayer = layers[1].getSubLayer(0);
+      // console.log(manholeLayer);
+      layers[1].on('featureOver', function(lat, long, pos, data, layerNumber) {
+      console.log(lat, long, pos, data, layerNumber);
+
+
       });
 
-    })
+      // let manholeButton = document.getElementById('manholes');
+
+      // $('#manholes').click(function() {
+      //   manholeLayer.setSQL('SELECT * FROM category WHERE category = "manhole"');
+      // });
+      //
+      // $('#reset').click(function() {
+      //   manholeLayer.setSQL('SELECT * FROM category');
+      // });
+      // let resetButton = document.getElementById('reset');
+      // a
+      // resetButton.addEventListener("click", function() {
+      //   manholeLayer.setSQL('SELECT * FROM category')
+      // });
+
+
+    });
 };
